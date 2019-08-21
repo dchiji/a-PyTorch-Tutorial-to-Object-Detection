9c3188d3507dd8995f6ba045db8395395ad9bccf
Daiki Chijiwa <daiki41@gmail.com>
2019-08-21 21:59:58 +0900

    fix data_folder again

--- create_data_lists.py
+++ create_data_lists.py
@@ -3,4 +3,4 @@ from utils import create_data_lists
 if __name__ == '__main__':
     create_data_lists(voc07_path='data/VOCdevkit/VOC2007',
                       voc12_path='data/VOCdevkit/VOC2012',
-                      output_folder='save')
+                      output_folder='data')
